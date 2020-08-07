84f506a9440640d39ecb41cfda8a04fa4e851c86
Gabriel10Velloso <gavelloso@gmail.com>
2020-08-06 21:03:00 -0300

    pegando os dados do usuário

--- back/routes/post.ts
+++ back/routes/post.ts
@@ -10,21 +10,21 @@ const fileSystem = new FileSystem();
 
 // Obtener POST paginados
 postRoutes.get('/', async (req: any, res: Response) => {
-  
+
   let pagina = Number(req.query.pagina) || 1;
   let skip = pagina - 1;
   skip = skip * 10;
 
   const posts = await Post.find()
-                          .sort({ _id: -1 })
-                          .skip( skip )
-                          .limit(10)
-                          .populate('usuario', '-password')
-                          .exec();
+    .sort({ _id: -1 })
+    .skip(skip)
+    .limit(10)
+    .populate('usuario', '-password')
+    .exec();
   res.json({
-      ok: true,
-      pagina,
-      posts
+    ok: true,
+    pagina,
+    posts
   });
 });
 
@@ -35,8 +35,8 @@ postRoutes.post('/', [verificaToken], (req: any, res: Response) => {
   const body = req.body;
   body.usuario = req.usuario._id;
 
-  // const imagenes = fileSystem.imagenesDeTempHaciaPost(req.usuario._id);
-  // body.imgs = imagenes;
+  const imagenes = fileSystem.imagenesDeTempHaciaPost(req.usuario._id);
+  body.imgs = imagenes;
 
   Post.create(body).then(async postDB => {
     await postDB.populate('usuario', '-password').execPopulate();
@@ -51,37 +51,46 @@ postRoutes.post('/', [verificaToken], (req: any, res: Response) => {
 
 
 // Servicio para subir archivos
-postRoutes.post( '/upload', [ verificaToken ], async (req: any, res: Response) => {
-    
-  if ( !req.files ) {
-      return res.status(400).json({
-          ok: false,
-          mensaje: 'No se subió ningun archivo'
-      });
+postRoutes.post('/upload', [verificaToken], async (req: any, res: Response) => {
+
+  if (!req.files) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: 'No se subió ningun archivo'
+    });
   }
 
   const file: FileUpload = req.files.image;
 
-  if ( !file ) {
-      return res.status(400).json({
-          ok: false,
-          mensaje: 'No se subió ningun archivo - image'
-      });
+  if (!file) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: 'No se subió ningun archivo - image'
+    });
   }
 
-  if ( !file.mimetype.includes('image') ) {
-      return res.status(400).json({
-          ok: false,
-          mensaje: 'Lo que subió no es una imagen'
-      }); 
+  if (!file.mimetype.includes('image')) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: 'Lo que subió no es una imagen'
+    });
   }
 
-  await fileSystem.guardarImagenTemporal( file, req.usuario._id );
+  await fileSystem.guardarImagenTemporal(file, req.usuario._id);
 
   res.json({
-      ok: true,
-      file: file.mimetype
+    ok: true,
+    file: file.mimetype
   });
 
 });
+
+
+postRoutes.get('/imagen/:userid/:img', (req: any, res: Response) => {
+  const userId = req.params.userid;
+  const img = req.params.img;
+  const pathFoto = fileSystem.getFotoUrl(userId, img);
+  res.sendFile(pathFoto);
+});
+
 export default postRoutes;
