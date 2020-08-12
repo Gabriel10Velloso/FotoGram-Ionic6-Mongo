f9fde5b7193a7b17fc858cfce1de291d87dbe076
Gabriel10Velloso <gavelloso@gmail.com>
2020-08-12 15:43:21 -0300

    Ajustes img

--- back/class/file-system2.ts
+++ back/class/file-system2.ts
@@ -4,16 +4,35 @@ import fs from 'fs';
 import uniqid from 'uniqid';
 
 export default class FileSystem2 {
-
+  newName: any = [];
+  nombreArchivo: any = [];
   constructor() { };
 
-  guardarImagenTemporal(file: FileUpload, userId: string) { 
+  guardarImagenTemporal(file: FileUpload, userId: string) {
     const path = this.crearCarpetaUsuario(userId);
 
+    // Nombre archivo
+    const nombre = this.generarNombreUnico(file);
+    console.log('ttttttttttt',  nombre);
   }
 
-  private generarNombreUnico(nombreOriginal: string) { 
-
+  private generarNombreUnico(nombreOriginal: any) {
+    
+    if (nombreOriginal.length == undefined) {
+      const nombreArr = nombreOriginal.name.split('.');
+      const extension = nombreArr[nombreArr.length - 1];
+      const idUnico = uniqid();
+      this.newName.push({image: `${idUnico}.${extension}`})
+      return this.newName 
+    }
+    nombreOriginal.forEach((item: any, i: any) => {
+      // 6.copy.jpg []
+      const nombreArr = item.name.split('.');
+      const extension = nombreArr[nombreArr.length - 1];
+      const idUnico = uniqid();
+      this.newName.push({image: `${idUnico}.${extension}`})
+    });
+    return this.newName
   }
 
 
--- back/dist/class/file-system2.js
+++ back/dist/class/file-system2.js
@@ -5,13 +5,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
+const uniqid_1 = __importDefault(require("uniqid"));
 class FileSystem2 {
-    constructor() { }
+    constructor() {
+        this.newName = [];
+        this.nombreArchivo = [];
+    }
     ;
     guardarImagenTemporal(file, userId) {
         const path = this.crearCarpetaUsuario(userId);
+        // Nombre archivo
+        const nombre = this.generarNombreUnico(file);
+        console.log('ttttttttttt', nombre);
     }
     generarNombreUnico(nombreOriginal) {
+        if (nombreOriginal.length == undefined) {
+            const nombreArr = nombreOriginal.name.split('.');
+            const extension = nombreArr[nombreArr.length - 1];
+            const idUnico = uniqid_1.default();
+            this.newName.push({ image: `${idUnico}.${extension}` });
+            return this.newName;
+        }
+        nombreOriginal.forEach((item, i) => {
+            // 6.copy.jpg []
+            const nombreArr = item.name.split('.');
+            const extension = nombreArr[nombreArr.length - 1];
+            const idUnico = uniqid_1.default();
+            this.newName.push({ image: `${idUnico}.${extension}` });
+        });
+        return this.newName;
     }
     crearCarpetaUsuario(userId) {
         const pathUser = path_1.default.resolve(__dirname, '../uploads2/', userId);
